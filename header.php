99a5b05997fa171f296be7cb1de74aa0ec8c2a2a
Rick Komans <Rick Komans>
2017-06-12 13:19:48 +0200

    hoi

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Forum</title>
+    <link rel="stylesheet" href="CSS/index.css" type="text/css">
+</head>
+
+<body>
+
+<div id="menu">
+    <buton id="menubutt">Menu</buton>
+        <div id="menucont">
+            <a href="index.php">Home</a>
+            <a href="src/categories.php">Categories</a>
+            <a>Topics</a>
+            <a>Contact</a>
+            <a>24/7 Chat</a>
+        </div>
+</div>
